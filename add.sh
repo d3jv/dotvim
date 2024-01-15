845005675b86eacb0079ed8bf8a25432fa48750e
David Štorek <d.storek@seznam.cz>
2024-01-15 21:42:14 +0100

    Revert to using relative paths for submodules

--- add.sh
+++ add.sh
@@ -6,7 +6,7 @@ if [[ $# -ne 1 ]]; then
 fi
 
 BASENAME=$(echo -n "$1" | sed 's%^.*/\([^/]*\)\.git$%\1%g')
-PTH="$HOME/.vim/pack/plugins/start/$BASENAME"
+PTH="pack/plugins/start/$BASENAME"
 
 git submodule add --depth 1 "$1" "$PTH" && git config -f .gitmodules submodule."$PTH".shallow true
 vim -u NONE -c "helptags $PTH/doc" -c q
